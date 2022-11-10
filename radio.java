fd6af479835b145fc38c585071c3f12e25cade30
oscarfuentes22 <97373140+oscarfuentes22@users.noreply.github.com>
2022-11-10 08:44:55 -0600

    cambio de nombres de interfaces

--- radio.java
+++ radio.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-public class radio implements modo_estado, modo_radio, modo_volumen, productividad, reproduccion, telefono {
+public class radio implements modo_estado, modo_radio, modo_volumen, modo_productividad, modo_reproduccion, modo_telefono {
 
     private ArrayList<cancion> canciones;
     private ArrayList<contacto> contactos;
@@ -36,6 +36,111 @@ public class radio implements modo_estado, modo_radio, modo_volumen, productivid
 
     public void setEmisoras(ArrayList<double[]> emisoras) {
         this.emisoras = emisoras;
+    }
+
+
+    @Override
+    public void conexion(boolean conection) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void contactos(ArrayList<contacto> lista) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void llamar(ArrayList<contacto> lista) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void finalizar() {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void audio(Boolean flag) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void seleccionar(ArrayList<cancion> lista) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void cambiar_c(Boolean flag, ArrayList<cancion> lista) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void escuchar(ArrayList<cancion> lista) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public String viajes(String destino) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+    @Override
+    public void volumen(Boolean flag) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void cambiar(Boolean flag) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void cambiar_e(Boolean flag) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void guardar(ArrayList<Double> lista, Double emisora_number) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void cargar(ArrayList<Double> lista) {
+        // TODO Auto-generated method stub
+        
+    }
+
+
+    @Override
+    public void estado(Boolean state) {
+        // TODO Auto-generated method stub
+        
     }
 
     
